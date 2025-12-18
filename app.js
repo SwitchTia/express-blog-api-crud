a824690a178c22909da32bcd5f84e27cb3832e8c
SwitchTia <oblasser.tatiana@gmail.com>
2025-12-18 15:56:44 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+console.log("hey");
+import express from "express"
+import citiesRouter from "./routers/citiesRouter.js"
+
+
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.send("Server of my blog")
+})
+
+app.use("/cities", citiesRouter);
+
+
+app.listen(port, function () {
+  console.log("Server is connected on port " + port);
+});
+
