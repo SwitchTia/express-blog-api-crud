39edb56b524a3e8508dbcdedda4526dc5d414e32
SwitchTia <oblasser.tatiana@gmail.com>
2025-12-19 12:21:06 +0100

    Feat: tagsController wip

---
+++ controllers/tagsController.js
@@ -0,0 +1,53 @@
+import { cityBlogArray } from "../data.js";
+
+
+//Operations REST CRUD
+//INDEX
+function index(req, res) {
+   
+};
+
+
+//SHOW
+function show(req, res) {
+    
+}
+
+
+//STORE
+function store(req, res) {
+    
+}
+
+
+//UPDATE
+function update(req, res) {
+
+}
+
+
+//MODIFY
+function modify(req, res) {
+   
+}
+
+//DESTROY
+function destroy(req, res) {
+    
+}
+
+
+
+
+const cityController = {
+
+    // index,
+    // show,
+    // store,
+    // update,
+    // modify,
+    // destroy
+
+}
+
+export default cityController;
