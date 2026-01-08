201270425390d1123228aa23e122690f1bbaa7ff
SwitchTia <oblasser.tatiana@gmail.com>
2026-01-08 15:48:42 +0100

    Fix: not Found

--- middlewares/notFound.js
+++ middlewares/notFound.js
@@ -2,7 +2,7 @@ export default function notFound (req, res, next){
     res.status(404)
         .json({
             Error: "Not found",
-            message: "Page was not found"
+            message: `Page ${req.path} was not found`
         });
     next();
 } 
